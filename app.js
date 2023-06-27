7d878e72e2070bda75fac106a6f20382e85c0aa0
Alec Neevel <aneevel15@gmail.com>
2023-06-26 19:48:15 -0500

    Add command reader, executor

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+
+  const commandPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+  for (const file of commandFiles) 
+  {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[ERROR] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+
+  // There are interactions that are not slash commands, which we don't want to talk about
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true});
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  }
+  console.log(`Registered command ${interaction}`);
+});
 
 // When the client is ready, it is run
 client.once(Events.ClientReady, (c) => {
